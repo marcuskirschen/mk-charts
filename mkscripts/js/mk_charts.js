5b1ae003bd18082f93be2eb204a2ce25a2a0b3c1
mail <mail@mkirschen.de>
2021-02-28 17:24:37 +0100

    inital commit

---
+++ mkscripts/js/mk_charts.js
@@ -0,0 +1,24 @@
+function createCircleChart(percent, color, size, stroke) {
+    let svg = `<svg class="mkc_circle-chart" viewbox="0 0 36 36" width="${size}" height="${size}" xmlns="http://www.w3.org/2000/svg">
+        <path class="mkc_circle-bg" stroke="#eeeeee" stroke-width="${stroke * 0.5}" fill="none" d="M18 2.0845
+              a 15.9155 15.9155 0 0 1 0 31.831
+              a 15.9155 15.9155 0 0 1 0 -31.831"/>
+        <path class="mkc_circle" stroke="${color}" stroke-width="${stroke}" stroke-dasharray="${percent},100" stroke-linecap="round" fill="none"
+            d="M18 2.0845
+              a 15.9155 15.9155 0 0 1 0 31.831
+              a 15.9155 15.9155 0 0 1 0 -31.831" />
+        <text class="mkc_info" x="50%" y="50%" alignment-baseline="central" text-anchor="middle" font-size="8">${percent}%</text>
+    </svg>`;
+    return svg;
+}
+
+let charts = document.getElementsByClassName('mkCharts');
+
+for(let i=0;i<charts.length;i++) {
+    let chart = charts[i];
+    let percent = chart.dataset.percent;
+    let color = ('color' in chart.dataset) ? chart.dataset.color : "#2F4F4F";
+    let size = ('size' in chart.dataset) ? chart.dataset.size : "100";
+    let stroke = ('stroke' in chart.dataset) ? chart.dataset.stroke : "1";
+    charts[i].innerHTML = createCircleChart(percent, color, size, stroke);
+}
